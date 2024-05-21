0a9fcf195312ea51aeefc63c1251bdf0ed3730f7
MukhammadAli <133798412+JavaDevALi@users.noreply.github.com>
2024-05-21 19:26:57 +0500

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
+import os
+
 from django.shortcuts import render, redirect
 from blog.models import Category, Post, Comment, Contact, Tag
 from django.core.paginator import Paginator
 from .validators import validate_contact_form
+import requests
+
+BOT_TOKEN = '7057112281:AAF-6Ok6aEuOz52ys6HbdaswCU27yzASInI'
+CHAT_ID = '330165400'
 
 
 def home_view(request):
@@ -36,6 +42,34 @@ def home_view(request):
             'tags': tags
         }
         return render(request, 'index.html', context=d)
+
+
+def category_view(request):
+    latest_posts = Post.objects.filter(is_published=True).order_by('-created_at')[:3]
+    categories = Category.objects.all()
+    cat_id = request.GET.get('cat')
+    page_number = request.GET.get('page', 1)
+    tags = Tag.objects.all()
+    top_posts = Post.objects.filter(is_published=True).order_by('-views')[3:8]
+
+    if cat_id:
+        category = Category.objects.filter(id=cat_id).first()
+        posts = Post.objects.filter(is_published=True, category_id=cat_id)
+    else:
+        category = None
+        posts = Post.objects.filter(is_published=True)
+
+    paginator = Paginator(posts, 2)
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'category.html', {
+        'posts': page_obj,
+        'categories': categories,
+        'category': category,
+        'latest_posts': latest_posts,
+        'tags': tags,
+        'top_posts': top_posts
+    })
 
 
 def about_view(request):
@@ -80,6 +114,18 @@ def contact_view(request):
                                          phone=data['phone'],
                                          message=data['message'])
             obj.save()
+            text = f"""
+                    project: Balita
+                    id:{obj.id}
+                    name:{obj.name}
+                    email:{obj.phone}
+                    subject:{obj.email}
+                    message:{obj.message}
+                    timestamp:{obj.created_at}
+                    """
+            url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text}'
+            response = requests.get(url)
+            print(response)
             return redirect('/contact')
         d['Error'] = validate['Error']
         return render(request, 'contact.html', context=d)
@@ -91,6 +137,9 @@ def blog_detail_view(request, pk):
         data = request.POST
         comment = Comment.objects.create(post_id=pk, name=data["name"], email=data["email"], message=data["message"])
         comment.save()
+        post = Post.objects.filter(id=pk).first()
+        post.comments_count += 1
+        post.save(update_fields=['comments_count'])
         return redirect(f'/blog/{pk}/')
     tags = Tag.objects.all()
     top_posts_main = Post.objects.filter(is_published=True).order_by('-views')[:3]
@@ -101,27 +150,12 @@ def blog_detail_view(request, pk):
     post.save(update_fields=['views'])
     comments = Comment.objects.filter(post_id=pk)
     categories = Category.objects.all()
+    related_posts = Post.objects.filter(is_published=True, category_id=post.category_id).exclude(pk=pk).order_by(
+        '-created_at')[:3]
     return render(request, 'blog-single.html',
                   {'post': post, 'comments': comments, 'categories': categories, 'latest_posts': latest_posts,
-                   'top_posts': top_posts, 'tags': tags, 'top_posts_main': top_posts_main})
-
-
-def category_view(request):
-    latest_posts = Post.objects.filter(is_published=True).order_by('-created_at')[:3]
-    categories = Category.objects.all()
-    cat = request.GET.get('cat')
-    tags = Tag.objects.all()
-    top_posts = Post.objects.filter(is_published=True).order_by('-views')[3:8]
-    if cat:
-        cat_name = categories.filter(id=cat).first()
-        posts = Post.objects.filter(is_published=True, category_id=cat)
-        return render(request, 'category.html',
-                      {'posts': posts, 'categories': categories, 'cat_name': cat_name, 'latest_posts': latest_posts,
-                       'tags': tags,'top_posts':top_posts})
-    else:
-        posts = Post.objects.filter(is_published=True)
-    return render(request, 'category.html',
-                  context={'posts': posts, 'categories': categories, 'tags': tags, 'latest_posts': latest_posts,'top_posts':top_posts})
+                   'top_posts': top_posts, 'tags': tags, 'top_posts_main': top_posts_main,
+                   'related_posts': related_posts})
 
 
 def search_view(request):
@@ -133,11 +167,11 @@ def search_view(request):
         return redirect(f'/search?q={query}')
     query = request.GET.get('q')
     if query is not None and len(query) >= 1:
-        posts = Post.objects.filter(is_published=True, title__icontains=query)
+        posts = Post.objects.filter(is_published=True, title__icontains=query).order_by('-created_at')
     else:
         posts = Post.objects.filter(is_published=True)
     return render(request, 'category.html',
-                  {'latest_posts': latest_posts, 'newest_posts': newest_posts,'tags': tags, 'posts': posts,
+                  {'latest_posts': latest_posts, 'newest_posts': newest_posts, 'tags': tags, 'posts': posts,
                    'categories': Category.objects.all()})
 
 
