c0dd7e9f60e888eb497964325dbb4c4910c83266
MukhammadAli <rasulovadildora3@gmail.com>
2024-07-11 15:50:31 +0500

    changes

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-    re_path(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    re_path(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    path('balita/admin/', admin.site.urls),
+    path('balita/', include('blog.urls')),
+    re_path(r'balita/static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'balita/media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
