ce985209a3ea41917752b63fada6fd127e020f8f
MukhammadAli <rasulovadildora3@gmail.com>
2024-09-25 13:21:48 +0500

    changes

--- blog/tests.py
+++ blog/tests.py
@@ -1,15 +1,1 @@
-def sqrt(x: int) -> int:
-    first = 0
-    last = x
-    if x == 0:
-        return 0
-    while first <= last:
-        mid = first + (last - first) // 2
-        if mid == x // mid:
-            return mid
-        elif mid > x // mid:
-            last = mid - 1
-        else:
-            first = mid + 1
-    return last
-
+adasdASF
