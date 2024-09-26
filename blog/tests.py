c62da50e214b1c47128e912f899024478a446460
MukhammadAli <rasulovadildora3@gmail.com>
2024-09-26 11:15:56 +0500

    changes

---
+++ blog/tests.py
@@ -0,0 +1,15 @@
+def sqrt(num):
+    first = 0
+    last = num
+    while first <= last:
+        mid = first + (last - first) // 2
+        if mid == num // mid:
+            return mid
+        elif mid > num // mid:
+            last = mid - 1
+        else:
+            first = mid + 1
+    return last
+
+
+print(sqrt(36))
