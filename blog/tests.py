c1f1a51ab00d7e539614f7e32e71b5f4bb00d855
MukhammadAli <rasulovadildora3@gmail.com>
2024-10-01 07:51:41 +0500

    changes

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,1 @@
-class Sqrt:
-    def sqrt(self, number):
-        first = 0
-        last = number
-        while first <= last:
-            middle = first + (last - first) // 2
-            if middle == number // middle:
-                return middle
-            elif middle > number // middle:
-                last = middle - 1
-            else:
-                first = middle + 1
-        return last
-
-
-print(Sqrt().sqrt(5))
+j
